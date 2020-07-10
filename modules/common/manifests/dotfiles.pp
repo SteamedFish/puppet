d68b5f386e029aa6c67c99153c10c6b05c318acf
SteamedFish <i@steamedfish.org>
2020-07-10 19:39:08 +0800

    add dotfiles

---
+++ modules/common/manifests/dotfiles.pp
@@ -0,0 +1,32 @@
+# configrations
+class common::dotfiles {
+    $homedir = $::kernel ? {
+        'Darwin' => '/Users/steamedfish',
+        default  => '/home/steamedfish',
+    }
+
+    vcsrepo { "${homedir}/.emacs.d":
+        ensure     => latest,
+        provider   => git,
+        source     => 'https://github.com/hlissner/doom-emacs.git',
+        revision   => 'develop',
+        submodules => true,
+    }
+
+    file { "${homedir}/.zinit":
+        ensure  => directory,
+        owner   => 'steamedfish',
+        group   => 'steamedfish',
+        mode    => '0755',
+        require => User['steamedfish'],
+    }
+
+    vcsrepo { "${homedir}/.zinit/bin":
+        ensure     => latest,
+        provider   => git,
+        source     => 'https://github.com/zdharma/zinit.git',
+        revision   => 'master',
+        submodules => true,
+        require    => File["${homedir}/.zinit"],
+    }
+}
--- modules/common/manifests/init.pp
+++ modules/common/manifests/init.pp
@@ -1,4 +1,5 @@
 # init
 class common inherits common::params {
     include common::users
+    include common::dotfiles
 }
