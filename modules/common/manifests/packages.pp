93f32c8f3b92acf7d03e94d01674a7d52717655c
SteamedFish <i@steamedfish.org>
2020-07-22 16:24:21 +0800

    setup basic packages

--- modules/common/manifests/packages.pp
+++ modules/common/manifests/packages.pp
@@ -1,5 +1,48 @@
 # configure packages
 class common::packages {
+
+    $packages = [
+        'htop',
+        'lftp',
+        'bash',
+        'zsh',
+        'bash-completion',
+        'dos2unix',
+        'figlet',
+        'toilet',
+        'git',
+        'hugo',
+        'exa',
+        'p7zip',
+        'ripgrep',
+        'telnet',
+        'unrar',
+        'unzip',
+        'wget',
+        'rsync',
+    ]
+
+    $packages_debian = ['toilet-fonts', 'fd-find', 'build-essential', 'bind9-host','bsdutils']
+
+    $packages_arch = ['toilet-fonts', 'youtube-dl', 'you-get', 'fd', 'lsd']
+
+    $packages_linux = ['dstat', 'dnsutils','busybox','diffutils','dmidecode','dnsutils','ansible-lint', 'ansible']
+
+
+    each($packages) |$package| {
+        package { $package:
+            ensure => installed,
+        }
+    }
+
+    if $facts['kernel'] == 'Linux' {
+        each($packages_linux) |$package| {
+            package { $package:
+                ensure => installed,
+            }
+        }
+    }
+
     if $facts['os']['family'] == 'Debian' {
         include apt
         class { 'apt':
@@ -9,5 +52,18 @@ class common::packages {
         }
 
         Class['apt::update'] -> Package <| provider == 'apt' |>
+
+        each($packages_debian) |$package| {
+            package { $package:
+                ensure => installed,
+            }
+        }
+    }
+    elsif $facts['os']['family'] == 'Archlinux' {
+        each($packages_arch) |$package| {
+            package { $package:
+                ensure => installed,
+            }
+        }
     }
 }
